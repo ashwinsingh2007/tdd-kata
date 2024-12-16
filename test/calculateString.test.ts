2306b0721b806b3b2d330b8689873649c080a5d2
ashwinsingh2007 <legend.ashwini07@gmail.com>
2024-12-16 17:13:40 +0530

    handle multiple number

--- test/calculateString.test.ts
+++ test/calculateString.test.ts
@@ -9,4 +9,13 @@ describe('String Calculator', () => {
     expect(add("1")).to.equal(1);
     expect(add("5")).to.equal(5);
   });
+  it('should return the sum of two numbers separated by a comma', () => {
+    expect(add("1,2")).to.equal(3);
+    expect(add("2,3")).to.equal(5);
+  });
+  it('should handle multiple numbers separated by commas', () => {
+    expect(add("1,2,3,4")).to.equal(10);
+    expect(add("10,20,30")).to.equal(60);
+  });
+  
 });
